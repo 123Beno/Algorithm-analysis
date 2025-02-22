b8315b01be79be32c84306e95b2db055239ebe05
123Beno <murimiben001@gmail.com>
2025-02-22 18:59:56 +0300

    Updated the running time .Added comments
    1.The factorial runtime grows linearly
    2.The fibonnaci sequence runtime grows exponentially.

--- factorial/Program.cs
+++ factorial/Program.cs
@@ -2,17 +2,17 @@
 
 class Program
 {
-    static void Main()
+    static void Main()//the main method
     {
-        int n = 5; // Input number
+        int n = 5; // Input number.
         int result = Factorial(n);
-        Console.WriteLine($"Factorial of {n} is: {result}");
+        Console.WriteLine($"Factorial of {n} is: {result}");//printing the output.
     }
 
-    static int Factorial(int n)
+    static int Factorial(int n)//the main function.
     {
         if (n == 0 || n == 1)
             return 1;
-        return n * Factorial(n - 1);
+        return n * Factorial(n - 1);//Runtime is N.It grows linearly.
     }
 }
--- fibonnaci/Program.cs
+++ fibonnaci/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-
+//The fibonnaci sequence is executed through recursion
 class Program
 {
-    static void Main()
+    static void Main()//the main method
     {
-        int n = 7; // Input number
+        int n = 7; // Input number.
         int result = Fibonacci(n);
-        Console.WriteLine($"Fibonacci number at position {n} is: {result}");
+        Console.WriteLine($"Fibonacci number at position {n} is: {result}");//printing the output
     }
 
-    static int Fibonacci(int n)
+    static int Fibonacci(int n)//the main function.
     {
         if (n <= 1)
             return n;
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
+        return Fibonacci(n - 1) + Fibonacci(n - 2);//The runtime is exponential.N^2
     }
 }
